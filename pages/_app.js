35c467cceb02a57f7fa5ca765edc8aefbeca19f3
Paolo <paolo@potatolondon.com>
2022-11-05 13:55:27 +0000

    Optimise leaf and remove analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,20 +5,6 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id="
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){window.dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-2N0535XLV9');
-      `}
-      </Script>
       <Component {...pageProps} />
     </>
   );
