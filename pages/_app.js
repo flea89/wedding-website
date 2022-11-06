343919a31e0c6060a79aa3d78402e77656934077
Paolo <paolo@potatolondon.com>
2022-11-06 11:08:31 +0000

    Import tachyons directly

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Script from "next/script";
 import "../styles/globals.scss";
+import "tachyons/css/tachyons.css";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -12,14 +12,10 @@ export default function Document() {
         />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;700&family=Libre+Franklin:wght@400;700&display=swap"
           rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
         />
       </Head>
       <body>
