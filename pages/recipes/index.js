d9964a493870ee04d94b79798a5bde529e15854a
Paolo <killer.paolo@gmail.com>
2023-03-26 18:17:05 +0100

    Manual redirect for recipe pages

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,9 +1,23 @@
 import Link from "next/link";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import RecipeLayout from "../../components/layouts/recipe";
 import { getAllRecipes, getPageCopy } from "../../utils/fsUtil";
+import { useRouter } from "next/router";
 
 export default function Index({ context, recipes }) {
+  // NextJS automatic locale detection works only on index pages.
+  // This is a quick workaround to detect the language on the client and redirect it to
+  // to the right page.
+  const router = useRouter();
+  useEffect(() => {
+    if (
+      !/^\/(it|en).*/.test(location.pathname) &&
+      /^en\b/.test(window.navigator.language)
+    ) {
+      router.push("/recipes", "/recipes", { locale: "en" });
+    }
+  }, []);
+
   return (
     <>
       <h1>{context.title}</h1>
